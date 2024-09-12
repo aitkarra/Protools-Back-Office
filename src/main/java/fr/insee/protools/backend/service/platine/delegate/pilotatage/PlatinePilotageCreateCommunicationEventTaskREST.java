c84e3e2a3b41d9185b5d7d7b12d6538196d44c28
SNDIL - Marc <marc.berger@insee.fr>
2024-09-12 15:11:44 +0200

    en cours sur ce fichier

--- src/main/java/fr/insee/protools/backend/service/platine/delegate/pilotatage/PlatinePilotageCreateCommunicationEventTaskREST.java
+++ src/main/java/fr/insee/protools/backend/service/platine/delegate/pilotatage/PlatinePilotageCreateCommunicationEventTaskREST.java
@@ -2,8 +2,7 @@ package fr.insee.protools.backend.service.platine.delegate.pilotatage;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import fr.insee.protools.backend.dto.platine.pilotage.PlatinePilotageCommunicationEventDto;
-import fr.insee.protools.backend.service.DelegateContextVerifier;
-import fr.insee.protools.backend.service.context.ContextService;
+import fr.insee.protools.backend.exception.ProtoolsProcessFlowBPMNError;
 import fr.insee.protools.backend.service.platine.service.PlatinePilotageService;
 import fr.insee.protools.backend.service.utils.FlowableVariableUtils;
 import lombok.RequiredArgsConstructor;
@@ -21,7 +20,7 @@ import static fr.insee.protools.backend.service.FlowableVariableNameConstants.*;
 @Slf4j
 @RequiredArgsConstructor
 @Component
-public class PlatinePilotageCreateCommunicationEventTaskREST implements JavaDelegate, DelegateContextVerifier {
+public class PlatinePilotageCreateCommunicationEventTaskREST implements JavaDelegate {
 
     private final PlatinePilotageService platinePilotageService;
 
@@ -35,10 +34,38 @@ public class PlatinePilotageCreateCommunicationEventTaskREST implements JavaDele
         List<JsonNode> contentList = FlowableVariableUtils.getVariableOrThrow(execution, VARNAME_REM_INTERRO_LIST, List.class);
         Map<String,String> communicationRequestIdByInterroIdMap = FlowableVariableUtils.getVariableOrThrow(execution, VARNAME_COMMUNICATION_REQUEST_ID_FOR_INTERRO_ID_MAP, Map.class);
 
-          //TODO: put it at a single place ==> Maybe an helper to get the Id of an interro or of any json?
+        if(currentCommunicationId.isBlank()){
+            log.error("ProcessInstanceId={}  currentPartitionId={} - currentCommunicationId{} : currentCommunicationId cannot be blank",
+                    execution.getProcessInstanceId(),currentPartitionId, currentCommunicationId);
+            throw new ProtoolsProcessFlowBPMNError("PlatinePilotageCreateCommunicationEventTaskREST: communicationId cannot be empty");
+        }
+
+        //If nothing to do ==> Directly return
+        if(contentList.isEmpty()){
+            log.info("ProcessInstanceId={}  currentPartitionId={} - currentCommunicationId{} - end : Nothing to do",
+                    execution.getProcessInstanceId(),currentPartitionId, currentCommunicationId);
+            return;
+        }
+
+
+         //TODO: put it at a single place ==> Maybe an helper to get the Id of an interro or of any json?
         String jsonKeyId = "id";
 
         List<PlatinePilotageCommunicationEventDto> platinePilotageCommunicationEventList = contentList.stream()
+                .filter(jsonNode -> {
+                    JsonNode node = jsonNode.get(jsonKeyId);
+                    if(node==null || node.isMissingNode()|| node.asText().isBlank()){
+                        log.warn("ProcessInstanceId={}  currentPartitionId={}  : skipping an interro without id",execution.getProcessInstanceId(),currentPartitionId);
+                        return false;
+                    }
+                    else if(!communicationRequestIdByInterroIdMap.containsKey(node.asText())){
+                        log.warn("ProcessInstanceId={}  currentPartitionId={}  - interroID={} : cannot retrieve communicationRequestId",execution.getProcessInstanceId(),currentPartitionId,node.asText());
+                        return false;
+                    }
+                    else{
+                        return true;
+                    }
+                })
             .map(jsonNode -> {
                 String interroId = jsonNode.path(jsonKeyId).asText();
                 String communicationRequestId=communicationRequestIdByInterroIdMap.get(interroId);
@@ -48,6 +75,7 @@ public class PlatinePilotageCreateCommunicationEventTaskREST implements JavaDele
                         .interrogationId(interroId).state(COMMUNICATION_STATE_SENT).build();
             }).toList();
 
+        //Check ctx?
         platinePilotageService.postCommunicationEvent(platinePilotageCommunicationEventList);
 
         log.info("ProcessInstanceId={}  end",execution.getProcessInstanceId());
