87a59b3311ba354f9bd14f73aa2a918e5434976c
SNDIL - Marc <marc.berger@insee.fr>
2024-09-12 10:00:41 +0200

    tests: tests unitaires

---
+++ src/test/java/fr/insee/protools/backend/service/platine/delegate/questionnaire/PlatineQuestionnaireCreateInterrogationListTaskRESTTest.java
@@ -0,0 +1,7 @@
+package fr.insee.protools.backend.service.platine.delegate.questionnaire;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlatineQuestionnaireCreateInterrogationListTaskRESTTest {
+
+}
